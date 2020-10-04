b542f844e68996882750823a27099d0596e4c6e7
kkingfung <kevinkfsun@gmail.com>
2020-10-04 22:12:39 +0900

    Preparation

--- RandomTowerDefense/Assets/Scripts/Managers/WaveManager.cs
+++ RandomTowerDefense/Assets/Scripts/Managers/WaveManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class WaveManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

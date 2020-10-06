e341bae6f9b208cbf9b30fca6cc1a606d3cfd865
kkingfung <kevinkfsun@gmail.com>
2020-10-06 20:00:57 +0900

    NewShader

--- RandomTowerDefense/Assets/Scripts/Managers/DebugManager.cs
+++ RandomTowerDefense/Assets/Scripts/Managers/DebugManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageSelectOperation : MonoBehaviour
+public class DebugManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class StageSelectOperation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }

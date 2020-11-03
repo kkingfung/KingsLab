2a47b48e5336e872cf5cc7087995e3c048e4a9b4
kkingfung <kevinkfsun@gmail.com>
2020-11-03 22:05:46 +0900

    PreparationForUIElements

---
+++ RandomTowerDefense/Assets/Editor/CustomEditorInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+//[CustomEditor(typeof(),true)]
+public class CustomEditorInspector : Editor
+{
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement container = new VisualElement();
+
+        SerializedProperty it = serializedObject.GetIterator();
+        it.Next(true);
+
+        while (it.NextVisible(false))
+        {
+            PropertyField prop = new PropertyField(it);
+            prop.SetEnabled(it.name != "m_Script");
+            container.Add(prop);
+        }
+
+        return container;
+    }
+}
